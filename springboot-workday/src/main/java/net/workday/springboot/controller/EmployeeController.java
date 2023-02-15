b79f4f505a8bd3ca52501f21662745e6095e36eb
Daehan Lee <dlee739@uwo.ca>
2023-02-15 16:49:09 -0500

    initial commit

---
+++ springboot-workday/src/main/java/net/workday/springboot/controller/EmployeeController.java
@@ -0,0 +1,80 @@
+package net.workday.springboot.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.workday.springboot.exception.ResourceNotFoundException;
+import net.workday.springboot.model.Employee;
+import net.workday.springboot.repository.EmployeeRepository;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// retrieve all employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+	
+	// create employee REST API
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+//		Optional<Employee> existingEmployee = employeeRepository.findByEmail(employee.getEmail());
+//	    if (existingEmployee.isPresent()) {
+//	      throw new DuplicateEmployeeException("Employee with email " + employee.getEmail() + " already exists.");
+//	    }
+		return employeeRepository.save(employee);
+	}
+	
+	// get employee by id REST API
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+		return ResponseEntity.ok(employee);
+	}
+	
+	// update employee by REST API
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+		
+		// update
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updatedEmployee = employeeRepository.save(employee);
+		
+		return ResponseEntity.ok(updatedEmployee);
+		
+	}
+	
+	// delete employee REST API
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+		
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+}
---
+++ springboot-workday/src/test/java/net/workday/springboot/SpringbootWorkdayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.workday.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWorkdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
